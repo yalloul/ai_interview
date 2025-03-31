90de4baa0855a79511942015550ce7565f4a1d77
younes alloul <younesalloul64@gmail.com>
2025-03-31 22:51:10 +0200

    auth and firebase

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,17 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}:{children: ReactNode  }) => {
     return (
-        <div>
+        <div className='root-layout'> 
+            <nav>
+                <Link href="/" className='flex items-center gap-2'>
+                <Image src="/logo.svg" alt="logo" width={38} height={32} /></Link>
+                
+            </nav>
             {children}
-        </div>
+       </div>
     )
 }
 
---
+++ firebase/client.ts
@@ -0,0 +1,22 @@
+
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCighKblwWDTSbwMZwHom-hVHDhm4-DCf0",
+  authDomain: "aiinterview-5be47.firebaseapp.com",
+  projectId: "aiinterview-5be47",
+  storageBucket: "aiinterview-5be47.firebasestorage.app",
+  messagingSenderId: "81428893899",
+  appId: "1:81428893899:web:94f37f3a3a729dfe624b4e",
+  measurementId: "G-HLF7JXK6EB"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
